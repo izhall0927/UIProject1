15e87b88a5e705586e1eeb54f693859ec1a93b90
izhall0927 <85722366+izhall0927@users.noreply.github.com>
2023-10-06 20:05:14 -0400

    fixed naming

--- js/home.js
+++ js/home.js
@@ -6,16 +6,16 @@ const recipeTab = document.getElementById("recipeTab");
 // Add click event listeners to the tabs
 homeTab.addEventListener("click", function () {
     // Navigate to the home page
-    window.location.href = "/Users/isabellahall/Downloads/basic-template/tabs/home.html";
+    window.location.href = "/Users/isabellahall/Downloads/UIProject1-1/tabs/home.html";
 });
 
 openIngredientsTab.addEventListener("click", function () {
     // Navigate to the Ingredients Pantry page
-    window.location.href = "/Users/isabellahall/Downloads/basic-template/tabs/recipesearch.html";
-    window.location.href = "/Users/isabellahall/Downloads/basic-template/tabs/ingredientspantry.html";
+    window.location.href = "/Users/isabellahall/Downloads/UIProject1-1/tabs/recipesearch.html";
+    window.location.href = "/Users/isabellahall/Downloads/UIProject1-1/tabs/ingredientspantry.html";
 });
 
 recipeTab.addEventListener("click", function () {
     // Navigate to the Recipe Search page
-    window.location.href = "/Users/isabellahall/Downloads/basic-template/tabs/recipesearch.html";
+    window.location.href = "/Users/isabellahall/Downloads/UIProject1-1/tabs/recipesearch.html";
 });
